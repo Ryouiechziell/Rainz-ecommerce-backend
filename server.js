815c7cdc5346a5d5ba2e1bb98f88f90dd1abe0ec
Nazril <73063248+Ryouiechziell@users.noreply.github.com>
2025-07-06 19:18:16 +0700

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const app = require("./src/main");
-const db = require(".src/models/models");
-const PORT = process.env.PORT || 7000;
-
-db.sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
-});
